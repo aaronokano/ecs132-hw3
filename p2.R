57e077140f5db95259beebb3445e3cefff6b554d
Aaron Okano <adokano@ucdavis.edu>
2012-10-30 22:54:57 -0700

    Indentation was weird

--- p2.R
+++ p2.R
@@ -1,13 +1,13 @@
 rdist <- function( n ) {
-    return( 1 / ( 1 - runif( n ) ) )
+  return( 1 / ( 1 - runif( n ) ) )
 }
 
 p2 <- function( n ) {
-    vec <- rdist( n )
+  vec <- rdist( n )
   ex  <- sum( sqrt( vec ) ) / n
-    px  <- length( vec[ vec > 3.0 ] ) / n
-    print( ex )
-      print( px )
+  px  <- length( vec[ vec > 3.0 ] ) / n
+  print( ex )
+  print( px )
 }
 
 p2( 100000 )
